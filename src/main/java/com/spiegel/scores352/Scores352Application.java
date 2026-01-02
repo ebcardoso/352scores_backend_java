f68d11d6c3c83513386ae976cf6190ef0d748eb1
Breno Cardoso <elisiobreno@gmail.com>
2026-01-01 23:44:20 -0300

    api-1: Uploading project to git

---
+++ src/main/java/com/spiegel/scores352/Scores352Application.java
@@ -0,0 +1,13 @@
+package com.spiegel.scores352;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scores352Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Scores352Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spiegel/scores352/Scores352ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiegel.scores352;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scores352ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
